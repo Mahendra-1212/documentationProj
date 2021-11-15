e9fb41b22ddea7d2a5d8f40dad304cc601ce5893
unknown <mahendrasahu1212@gmail.com>
2021-11-15 13:05:17 +0530

    port number issue fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
 const dateformat=require('date-format');
-const PORT=process.env.PORT||"3000";
+const PORT=process.env.PORT||4000;
 
 app.get("/api/v1/instagram",(req,res)=>{
     console.log();
